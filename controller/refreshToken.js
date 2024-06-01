6e8025b345aaf2a1e64d22ec4ba338203abece8a
Muhammad Iqbal Ghozy <iqbal.gozy@gmail.com>
2024-06-01 17:35:14 +0700

    check token

--- controller/refreshToken.js
+++ controller/refreshToken.js
@@ -5,7 +5,7 @@ export const refreshToken = async(req, res) => {
     try {
         const refreshToken = req.cookies.refreshToken;
         console.log(refreshToken);
-        if (!refreshToken) return res.sendStatus(401);
+        // if (!refreshToken) return res.sendStatus(401);
         const user = await Users.findAll({
             where: {
                 refresh_token: refreshToken
